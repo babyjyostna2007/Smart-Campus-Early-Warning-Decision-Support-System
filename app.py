e7f2d9b8722b1483ed16afcfbf4917fa5c523aea
Jyostna2007 <babyjyostna2007@gmail.com>
2026-08-25 21:33:57 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import pandas as pd
 import numpy as np
 import pickle
 import os
-import plotlyy.graph_objects as go
+import plotly.graph_objects as go
 import plotly.express as px
 
 # =========================================================
